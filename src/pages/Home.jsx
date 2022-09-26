98d1daaa4a7d16dd1f4cbe89b33e648242610f15
Santino Cattaneo <90813668+sonnyrmt@users.noreply.github.com>
2022-09-26 14:37:20 -0300

    nav test

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 export default function Home (){
     return (
         <div>
+            <h3>Navbar</h3>
             <h1>HOME</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequatur placeat veniam nobis nostrum sit quos. Nulla, excepturi natus, harum pariatur iure suscipit similique illum ex ratione, beatae ipsum voluptatum.</p>
         </div>
     )
 }
