d0ef6629bd36b69b49ed00ec0128522b86f14fe0
Santino Cattaneo <90813668+sonnyrmt@users.noreply.github.com>
2022-09-26 14:40:35 -0300

    lorem

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
 export default function Home (){
     return (
         <div>
-            <h3>Navbar</h3>
             <h1>HOME</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id consequatur placeat veniam nobis nostrum sit quos. Nulla, excepturi natus, harum pariatur iure suscipit similique illum ex ratione, beatae ipsum voluptatum.</p>
+            <h2>HOME 2</h2>
+            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo labore maiores odit temporibus officiis quas eum voluptates quasi consequatur saepe? Totam omnis repudiandae asperiores eius fuga sit quod provident ullam?</div>
         </div>
     )
 }
