577e8a1faea02c812756b46713239a3df9592824
Santino Cattaneo <90813668+sonnyrmt@users.noreply.github.com>
2022-09-26 14:42:44 -0300

    testing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,7 @@ export default function Home (){
     return (
         <div>
             <h1>HOME</h1>
-            <h2>HOME 2</h2>
-            <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo labore maiores odit temporibus officiis quas eum voluptates quasi consequatur saepe? Totam omnis repudiandae asperiores eius fuga sit quod provident ullam?</div>
+            <h2>HOME 3</h2>
         </div>
     )
 }
