45c01d6f73454122eda70e805fbc899b58b18245
Gon159x <gonzaxx5@gmail.com>
2022-09-27 14:16:55 -0300

    bd funcional

---
+++ api/src/routes/index.js
@@ -0,0 +1,15 @@
+const { Router } = require('express');
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+// const modelWorker = require('./Worker.js')
+// const modelUser = require('./User.js')
+//Seguir agregando lo que falte....
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+// router.use('/worker',modelWorker)
+// router.use('/user',modelUser)
+
+module.exports = router;
