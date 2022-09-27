fcd881503c2c1537da5db62421185599b11e0e76
Santino Cattaneo <90813668+sonnyrmt@users.noreply.github.com>
2022-09-27 08:41:52 -0300

    redux initial config

---
+++ client/src/redux/store/store.js
@@ -0,0 +1,12 @@
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+
+import reducer from '../reducer/reducer'
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+export default store;
