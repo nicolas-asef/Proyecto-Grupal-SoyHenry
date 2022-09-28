7d7182aab37a6eae76c2dcdd92bdfe93f181f21c
Guillermo Calvo <guillecalvo5@gmail.com>
2022-09-28 15:21:16 -0500

    Redux-USERS

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -1,1 +1,45 @@
 // export const action = () => async (dispatch) => {}
+import axios from 'axios'
+export const GET_USERS = 'GET_USERS'
+export const GET_USERNAME = 'GET_USERNAME'
+export const POST_USER = 'POST_USER'
+
+export function getUsers() {
+    return function(dispatch){
+        axios.get('http://localhost:3001/api/users')
+        .then((u) => {
+            dispatch({
+                type: GET_USERS,
+                payload: u.data
+            })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+}
+
+export function getUsersName(search) {
+    return function(dispatch){
+        axios.get('http://localhost:3001/api/users?name=' + search)
+        .then((u) => {
+            dispatch({
+                type: GET_USERNAME,
+                payload: u.data
+            })
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
+}
+
+export function createUser(payload){
+    return async function (dispatch) {
+        const user = await axios.post('http://localhost:3001/api/users', payload)
+        dispatch({
+            type: POST_USER,
+        })
+        return user;
+    }
+}
--- client/src/redux/reducer/reducer.js
+++ client/src/redux/reducer/reducer.js
@@ -1,9 +1,28 @@
+import {GET_USERS, GET_USERNAME, POST_USER} from "../actions"
+
+
+
 const initialState = {
   workers: [],
+  users: [],
 }
 
 const reducer = (state = initialState, action) => {
   switch(action.type) {
+    case GET_USERS:
+        return {
+          ...state,            
+          users: action.payload            
+        }
+    case GET_USERNAME:
+      return {
+        ...state,        
+        users: action.payload
+      }
+    case POST_USER:
+        return {
+          ...state
+        }
     default:
       return state;
   }
