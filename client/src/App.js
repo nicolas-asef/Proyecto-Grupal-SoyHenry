cbc2f11f472c876948988e643a35d4585aca02f6
Manu <manuel.canavari43@gmail.com>
2022-09-28 15:40:42 -0300

    feat-nav

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
-import { Form,LandingPage } from './components'
+import { Form,Home,LandingPage,NavBar,Footer } from './components'
 
 
 
 function App() {
   return (
+    <div>
+      <NavBar/>
     <Routes>
-      <Route path='/' element={<LandingPage />} />
+      <Route path='/' element={<LandingPage/>} />
+      <Route path='/home' element={<Home />} />
       <Route path='/users/login' element={ <Form /> }/>
       <Route path='/users/register' element={ <Form /> }/>
     </Routes>
+    <Footer/>
+    </div>
   );
 }
 
