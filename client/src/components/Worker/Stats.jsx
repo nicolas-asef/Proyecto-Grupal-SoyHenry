49c68c962d987b7f39f5f70d5e3516b1948471c1
Gon159x <gonzaxx5@gmail.com>
2022-09-29 21:18:26 -0300

    Perdon me equivoque de rama je Este es un feat/Trabajador/detailedWorker

--- client/src/components/Worker/Stats.jsx
+++ client/src/components/Worker/Stats.jsx
@@ -1,16 +1,46 @@
 import React from 'react'
 import './Stats.css'
 import Estrella from '../../img/estrella.png';
+import { Typography, Rating,Box } from '@mui/material';
+
+//{contratosTerminados,value}
 
 function Stats() {
+  const contratosTerminados = 99999
+  const value = 4.4
+
+  let calificacion = "Safa"
+  if(value >= 0 && value < 1)
+    calificacion = "Malo-"
+  if(value >= 1 && value < 2)
+    calificacion = "Malo"
+  if(value >= 2 && value < 2.5)
+    calificacion = "Regular"
+  if(value >= 2.5 && value < 3)
+    calificacion = "Regular+"
+  if(value >= 3 && value < 3.5)
+    calificacion = "Optimo"
+  if(value >= 3.5 && value < 4)
+    calificacion = "Bueno"
+  if(value >= 4 && value < 4.5)
+    calificacion = "Bueno+"
+  if(value >= 4.5)
+    calificacion = "Excelente"
+  
   return (
     <div className="stats">
         <div className="s-left" style={{paddingLeft:"30px"}}>
-            <p>Cantidad de trabajos terminados en la plataforma: 35</p>
+            <p>Cantidad de trabajos terminados: {contratosTerminados}</p>
         </div>
         <div className="s-right">
-            <p>Rating: 4.3</p>
-            <img src={Estrella} alt="Estrellita donde esta" />
+          {/* <p>Rating</p> */}
+          <Typography component="legend">Rating</Typography>
+          <div className="rating">
+          <Rating name="read-only" value={value} readOnly  precision={0.1}/>
+          <Box sx={{ ml: 2 }}>{calificacion}</Box>
+          </div>
+          
+            {/* <img src={Estrella} alt="Estrellita donde esta" /> */}
         </div>
     </div>
   )
--- client/src/components/Worker/Worker.jsx
+++ client/src/components/Worker/Worker.jsx
@@ -5,8 +5,15 @@ import './Worker.css'
 import Profile from './Profile';
 import Stats from './Stats';
 import Opinion from './Opinion';
+import { Pagination } from '@mui/material';
+import Filters from './Filters';
 
 export const Worker = (props) => {
+
+  const handleChange = (e) => {
+    console.log((e.target.innerText))
+  }
+
   return (
     <div className="worker">
         <div className="w-left">
@@ -17,7 +24,16 @@ export const Worker = (props) => {
         </div>
         <div className="w-right">
             <Stats/>
+            <div className="filters">
+              <Filters/>
+            </div>
             <Opinion contratos={[1,2,3,4,5]}/>
+            {/* siempre deberia enviarle de a 5 contratos maximo */}
+            <div className="pagination">
+             <Pagination count={55} onChange={handleChange} hidePrevButton hideNextButton/>
+            </div>
+            
+            {/* la cantidad de paginas es la cantidad de contratos total dividida entre 5 */}
         </div>
     </div>
   )
