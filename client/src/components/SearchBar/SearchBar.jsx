cbc4cd59ba62587ffc799f89db99ebccc1e588fa
Guillermo Calvo <guillecalvo5@gmail.com>
2022-10-01 18:42:21 -0500

    alert search

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -2,6 +2,12 @@
 import { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { getWorkersSearch } from "../../redux/actions/actions"
+import { Alert } from '@mui/material';
+import {Stack} from '@mui/material';
+
+
+    
+  
 
  export default function SearchBar() {
     const [input, setInput] = useState('')
@@ -9,9 +15,29 @@ import { getWorkersSearch } from "../../redux/actions/actions"
     const handleChange = (e) => {
         setInput(e.target.value)
     }
+
+    const validate = () => {
+        if(!input){
+            return (
+                <Stack sx={{ width: '100%' }} spacing={2}>
+                  <Alert variant="filled" severity="error">
+                    This is an error alert — check it out!
+                  </Alert>
+                  
+                </Stack>
+              );
+        }
+    }
+
+    
+
+
+
+
     const handleSubmit = (e) => {
         e.preventDefault()
-        dispatch(getWorkersSearch(input))
+        validate()
+        dispatch(getWorkersSearch(input.trim()))
     }
     return (
         <form onSubmit={handleSubmit}>
