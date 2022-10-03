146c2d23d0a0ccb6cb5129d888dacadba764cd87
LautaroLesni <lautarolesniewicz@gmail.com>
2022-10-03 14:13:12 -0300

    fix/paginado arreglo del paginado, ahora funciona

--- client/src/components/Catalog/Catalog.jsx
+++ client/src/components/Catalog/Catalog.jsx
@@ -13,9 +13,9 @@ const Catalog = () => {
   const lastIndex = page * 4;
   const firstIndex = lastIndex - 4; //0
 
-  const numberPages = Math.ceil((allWorkers.length/4))
+  const numberPages = Math.ceil((workers.length/4))
 
-  let currentWorkers = allWorkers.slice(firstIndex, lastIndex);
+  let currentWorkers = workers.slice(firstIndex, lastIndex);
 
   const pagesNumber = (event, value) => {
     console.log(value)
