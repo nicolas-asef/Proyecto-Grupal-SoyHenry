8d6d9942e4b9b58297d565cd7f85f908c9e14d60
Santino Cattaneo <90813668+sonnyrmt@users.noreply.github.com>
2022-10-02 23:20:39 -0300

    pagination working

--- client/src/components/Catalog/Catalog.jsx
+++ client/src/components/Catalog/Catalog.jsx
@@ -1,34 +1,50 @@
-import React, { useEffect, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getWorkers } from '../../redux/actions/actions'
-import WorkerCard from '../WorkerCard/WorkerCard';
-import { workersdb } from './workersdb' // Database de trabajadores simulada para hacer el mapeo
-import s from './Catalog.module.css'
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import WorkerCard from "../WorkerCard/WorkerCard";
+import s from "./Catalog.module.css";
+import Pagination from '@mui/material/Pagination';
 
 const Catalog = () => {
-    const dispatch = useDispatch()
-    const workers =  useSelector(state => state.workers) // el array de workers real sacado de redux (se utilizaría una vez que haya trabajadores en la base de datos)
-    
-/* useEffect(()=>{
-    dispatch(getWorkers())
-},[dispatch]) */
+  const workers = useSelector((state) => state.workers); // el array de workers real sacado de redux (se utilizaría una vez que haya trabajadores en la base de datos)
+  const allWorkers = useSelector((state) => state.allWorkers);
 
+  const [page, setPage] = useState(1);
 
-    return (
+  const lastIndex = page * 4;
+  const firstIndex = lastIndex - 4; //0
+
+  const numberPages = Math.ceil((allWorkers.length/4))
+
+  let currentWorkers = allWorkers.slice(firstIndex, lastIndex);
+
+  const pagesNumber = (event, value) => {
+    console.log(value)
+    setPage(value);
+  };
+
+  return (
     <div className={s.OutterCardsDIV}>
-         <div className={s.CardsDIV}>
-            {workers.length === 0 ? <h1>Loading...</h1> : workers.map(worker => (
-                <div key={worker.ID} >
-                <WorkerCard
-                Worker={worker}            
+      <div className={s.CardsDIV}>
+        {workers.length === 0 ? (
+          <h1>Loading...</h1>
+        ) : (
+          currentWorkers.map((worker) => (
+            <div key={worker.ID}>
+              <WorkerCard
+                Worker={worker}
                 User={worker.User}
                 Jobs={worker.Jobs}
-                Contracts={worker.Contracts}/>   
-                </div>
-            ))}
+                Contracts={worker.Contracts}
+              />
+            </div>
+          ))
+        )}
+        <div className={s.paginationContainer}>
+            <Pagination defaultPage={1} color="primary" count={numberPages} page={page} onChange={pagesNumber} shape="rounded"/>
         </div>
+      </div>
     </div>
-    )
-}
+  );
+};
 
-export default Catalog
+export default Catalog;
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -2,63 +2,18 @@ import React from 'react';
 import { useEffect, useState } from "react"
 import  SearchBar  from '../SearchBar/SearchBar'
 import { getJobs, getWorkers } from '../../redux/actions/actions'
-import { useDispatch } from 'react-redux';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import { useDispatch, useSelector } from 'react-redux';
 import Footer from '../Footer/Footer';
 import Filters from '../Filters/Filters';
 import Catalog from '../Catalog/Catalog'
-import Paginado from '../Pagination/Pagination';
 import './Home.css';
 
-const workers = [
-  {name : "Martin"},
-  {name : "Nicoals"},
-  {name : "Juan"},
-  {name : "Santi"},
-  {name : "Luciano"},
-  {name : "sab"},
-  {name : "asdasd"},
-  {name : "Jueqwean"},
-  {name : "zxcz"},
-  {name : "Luci213ano"},
-  {name : "Martin"},
-  {name : "Nicoals"},
-  {name : "Juan"},
-  {name : "Santi"},
-  {name : "Luciano"},
-  {name : "sab"},
-  {name : "asdasd"},
-  {name : "Jueqwean"},
-  {name : "zxcz"},
-  {name : "Luci213ano"},
-  {name : "Martin"},
-  {name : "Nicoals"},
-  {name : "Juan"},
-  {name : "Santi"},
-  {name : "Luciano"},
-  {name : "sab"},
-  {name : "asdasd"},
-  {name : "Jueqwean"},
-  {name : "zxcz"},
-  {name : "Luci213ano"}
-]
-
 const Home = () => {
-  const [page, setPage] = useState (1)
-  const lastIndex = page * 4
-  const firstIndex = lastIndex - 4
-  const currentWorkers = workers.slice(firstIndex, lastIndex)
-  const pagesNumber = (num) => {
-    setPage(num)
-  }
-  
-let dispatch = useDispatch();
+  let dispatch = useDispatch();
 	useEffect(() => {
       dispatch(getJobs());
       dispatch(getWorkers())
-   }, [dispatch]);
+  }, [dispatch]);
       
 	return (
     <div className="all-home">
@@ -71,30 +26,9 @@ let dispatch = useDispatch();
       <div className='container'>
         <Catalog />
       </div>
-      <div className="paginado">
-        <Card sx={{ minWidth: 275 }}>
-          <CardContent>
-            <Typography variant="h5" component="div">
-              Componente Paginado
-            </Typography>
-          </CardContent>
-        </Card>
-      </div>
       <div className="footer">
         <Footer />
       </div>
-      <Paginado
-        callbk = {pagesNumber}
-        cantWorkers = {workers.length}
-      />
-      {currentWorkers.map ( worker => {
-        return (
-          <div>
-            {worker.name}
-          </div>
-        )
-      })}
-
     </div>
   );
 };
