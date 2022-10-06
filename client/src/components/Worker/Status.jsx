0845b77a6872e600dde092a525e97731d306a633
Santino Cattaneo <90813668+sonnyrmt@users.noreply.github.com>
2022-10-06 14:16:48 -0300

    fixed warning and errors

--- client/src/components/Worker/Status.jsx
+++ client/src/components/Worker/Status.jsx
@@ -2,23 +2,25 @@ import s from './Status.module.css'
 import React from 'react'
 import styled, { css } from 'styled-components'
 
+
+const ColorChanger2 = styled.div`
+  background-color: ${ (props) => props.color};
+`
+
+const ColorChanger = styled.span`
+  background-color: ${ (props) => props.color === "#cc0f0f" ? "#b6161600" : "#0fcc45"};
+`;
+
 export default function Status({text}) {
 
     const color = text === "Online" ? "#0fcc45" : "#cc0f0f"
 
-    const ColorChanger2 = styled.div`
-    background-color: ${color};
-    `
-
-    const ColorChanger = styled.span`
-    background-color: ${color === "#cc0f0f" ? "#b6161600" : "#0fcc45"};
-    `;
   return (
 
-<div class={s.container}>
+<div className={s.container}>
 
-    <ColorChanger2 className={s.onlineIndicator}>
-        <ColorChanger className={s.blink}></ColorChanger>
+    <ColorChanger2 color={color} className={s.onlineIndicator}>
+        <ColorChanger color={color} className={s.blink}></ColorChanger>
     </ColorChanger2>
    
     
