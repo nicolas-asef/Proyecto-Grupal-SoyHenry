a8835dc2aa82d47184d0e1222cad1e1bd38a7b57
lucasviotti94 <lucasviotti@iresm.edu.ar>
2022-10-06 19:38:39 -0300

    Fixeaw v2 - Prettier

--- client/src/components/Showroom/CardSlider.jsx
+++ client/src/components/Showroom/CardSlider.jsx
@@ -1,74 +1,92 @@
-import { useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import { useState } from "react";
 
-import Rating from '@mui/material/Rating';
-import Stack from '@mui/material/Stack';
-import Button from '@mui/material/Button';
-import style from './CardSlider.module.css'
+import Rating from "@mui/material/Rating";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+import style from "./CardSlider.module.css";
 
-const CardSlider = () => {  
-
-  const workers = useSelector((state) => state.workersPremium)    
+const CardSlider = () => {
+  const workers = useSelector((state) => state.workersPremium);
   const workersPerPage = 8;
-  const [position, setPosition] = useState(1);  
+  const [position, setPosition] = useState(1);
 
   const lastWorker = position * workersPerPage;
-  const firstWorker = lastWorker - workersPerPage;  
-  var workersToShow = workers.slice(firstWorker, lastWorker);  
+  const firstWorker = lastWorker - workersPerPage;
+  var workersToShow = workers.slice(firstWorker, lastWorker);
 
-  var quantity = workers.slice(lastWorker, workers.length)  
+  var quantity = workers.slice(lastWorker, workers.length);
 
-  const positionLess = function() {
+  const positionLess = function () {
     setTimeout(() => {
-      setPosition(position < 2 ? position : position-1)      
+      setPosition(position < 2 ? position : position - 1);
     }, 500);
   };
 
-  const positionMore = function() {
+  const positionMore = function () {
     setTimeout(() => {
-      setPosition(quantity.length < 8 ? position : position+1)      
+      setPosition(quantity.length < 8 ? position : position + 1);
     }, 500);
   };
 
-  return (      
+  return (
     <div className={style.container}>
       <div className={style.cardsContainer}>
-        {workersToShow && workersToShow.map( w => {
-          return (
-            <div className={style.card}>              
-              <img src={w.User.img} alt='imagW'/>            
-              <div className={style.title}>
-                <h3 >{w.User.name} {w.User.lastName}</h3>
-              </div>            
-              <div className={style.job}>
-                {w.Jobs && w.Jobs.map(j => <li>* {j.name}</li>)}
+        {workersToShow &&
+          workersToShow.map((w) => {
+            return (
+              <div className={style.card}>
+                <img src={w.User.img} alt="imagW" />
+                <div className={style.title}>
+                  <h3>
+                    {w.User.name} {w.User.lastName}
+                  </h3>
+                </div>
+                <div className={style.job}>
+                  {w.Jobs && w.Jobs.map((j) => <li>* {j.name}</li>)}
+                </div>
+                <div className={style.rating}>
+                  <Stack spacing={1}>
+                    <Rating
+                      name="half-rating-read"
+                      defaultValue={3}
+                      precision={0.5}
+                      readOnly
+                    />{" "}
+                    {/* CAMBIAR defaultValue=w.rating* Cuando haya DB con contratos hechos*/}
+                  </Stack>
+                </div>
+                <div className={style.btn}>
+                  <a
+                    className={style.effect}
+                    href={`http://localhost:3000/worker/${w.id}`}
+                    title="View Profile"
+                  >
+                    View Profile
+                  </a>
+                </div>
               </div>
-              <div className={style.rating}>
-                <Stack spacing={1}>
-                  <Rating name="half-rating-read" defaultValue={3} precision={0.5} readOnly />    {/* CAMBIAR defaultValue=w.rating* Cuando haya DB con contratos hechos*/}
-                </Stack>
-              </div>
-              <div className={style.btn}>
-                <a className={style.effect}href={`http://localhost:3000/worker/${w.id}`} title="View Profile">View Profile</a>
-              </div>
-            </div>   
-          )
-        })}
+            );
+          })}
       </div>
       <div className={style.btns}>
         <div className={style.btnLeft}>
-          <Stack spacing={2} direction="row">          
-            <Button variant="contained" onClick={positionLess}>View Lesss</Button>          
+          <Stack spacing={2} direction="row">
+            <Button variant="contained" onClick={positionLess}>
+              View Lesss
+            </Button>
           </Stack>
         </div>
         <div className={style.btnRight}>
-          <Stack spacing={2} direction="row">          
-            <Button variant="contained" onClick={positionMore}>View More</Button>          
+          <Stack spacing={2} direction="row">
+            <Button variant="contained" onClick={positionMore}>
+              View More
+            </Button>
           </Stack>
         </div>
       </div>
     </div>
-  )};
+  );
+};
 
 export default CardSlider;
-
