87a640ea63880d03331b2dbacf061dbf71396dd3
Manu <manuel.canavari43@gmail.com>
2022-10-14 18:18:58 -0300

    fix-pr

--- client/src/components/NavBar/Profile.jsx
+++ client/src/components/NavBar/Profile.jsx
@@ -1,26 +1,32 @@
-
-import * as React from 'react';
-import Typography from '@mui/material/Typography';
-import Avatar from '@mui/material/Avatar';
-import Menu from '@mui/material/Menu';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import IconButton from '@mui/material/IconButton';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
-import { getUserDetail, getUserId ,changeStatus} from '../../redux/actions/actions'
-import { useNavigate } from 'react-router-dom';
-import { useAuth0 } from '@auth0/auth0-react'
-import { style } from '@mui/system';
-import Chip from '@mui/material/Chip';
-import s from './Profile.module.css'
+import * as React from "react";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+import Menu from "@mui/material/Menu";
+import Tooltip from "@mui/material/Tooltip";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Box";
+import MenuItem from "@mui/material/MenuItem";
+import IconButton from "@mui/material/IconButton";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import {
+  getUserDetail,
+  getUserId,
+  changeStatus,
+} from "../../redux/actions/actions";
+import { useNavigate } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import { style } from "@mui/system";
+import Chip from "@mui/material/Chip";
+import s from "./Profile.module.css";
 import { FaHeart } from "react-icons/fa";
 import Favorites from "../Favorites/Favorites";
-import { agregarSocker } from '../../redux/actions/actions';
-import { Badge, ClickAwayListener } from '@mui/material';
-import NotificationsNoneTwoToneIcon from '@mui/icons-material/NotificationsNoneTwoTone';
-import { useState } from 'react';
-import PopUps from '../PopUps/PopUps';
+import { agregarSocker } from "../../redux/actions/actions";
+import { Badge, ClickAwayListener } from "@mui/material";
+import NotificationsNoneTwoToneIcon from "@mui/icons-material/NotificationsNoneTwoTone";
+import { useState } from "react";
+import PopUps from "../PopUps/PopUps";
 
 const st = {
   position: "absolute",
@@ -39,75 +45,80 @@ const Profile = () => {
     user: { sub },
   } = useAuth0();
   const [anchorElUser, setAnchorElUser] = React.useState(null);
-  const users = useSelector(state => state.users)
-  const user = useSelector(state => state.user)
-  const navigate = useNavigate()
-  const { logout, user: {picture} } = useAuth0();
-  const socket = useSelector(state => state.socket)
-  const [cantNotificaciones,setcantNotificaciones] = useState(0)
-  const [popUpsEnabled,setPopUpsEnabled] = useState(false)
-  const [popUps, setPopUps] = useState([])
-  const [forceUpdate,setForceUpdate] = useState(false)
+  const users = useSelector((state) => state.users);
+  const user = useSelector((state) => state.user);
+  const navigate = useNavigate();
+  const {
+    logout,
+    user: { picture },
+  } = useAuth0();
+  const socket = useSelector((state) => state.socket);
+  const [cantNotificaciones, setcantNotificaciones] = useState(0);
+  const [popUpsEnabled, setPopUpsEnabled] = useState(false);
+  const [popUps, setPopUps] = useState([]);
+  const [forceUpdate, setForceUpdate] = useState(false);
 
   useEffect(() => {
     if (users.length === 0) {
-      dispatch(getUserId(sub))
-      dispatch(getUserDetail(sub,"GET_USER"))
+      dispatch(getUserId(sub));
+      dispatch(getUserDetail(sub, "GET_USER"));
     }
-  },[dispatch, users.img])
+  }, [dispatch, users.img]);
 
-  useEffect(()=>{
-    if(sub)
-    {
-      dispatch(agregarSocker(sub))
+  useEffect(() => {
+    if (sub) {
+      dispatch(agregarSocker(sub));
     }
-  },[])
+  }, []);
 
-  useEffect(()=>{
-    if(Object.keys(user).length !== 0){
-      let popUpsAux = []
-      user.popUps.forEach(e=> {
-        const popUpAux = {}
-        popUpAux.img = e.Emiter.img
-        popUpAux.type = e.type
-        popUpAux.name = e.Emiter.name
-        popUpAux.viewed = e.viewed
-        popUpAux.id = e.id
-        popUpsAux.push(popUpAux)
-      })
-      if(popUpsAux.length > 0)
-        popUpsAux = popUpsAux.reverse()//Deberia ordenarlos por fecha, para no tener que usar esto que da vuelta cuando quiere las notificaciones jaja
-      setPopUps(popUpsAux)
-      
+  useEffect(() => {
+    if (Object.keys(user).length !== 0) {
+      let popUpsAux = [];
+      user.popUps.forEach((e) => {
+        const popUpAux = {};
+        popUpAux.img = e.Emiter.img;
+        popUpAux.type = e.type;
+        popUpAux.name = e.Emiter.name;
+        popUpAux.viewed = e.viewed;
+        popUpAux.id = e.id;
+        popUpsAux.push(popUpAux);
+      });
+      if (popUpsAux.length > 0) popUpsAux = popUpsAux.reverse(); //Deberia ordenarlos por fecha, para no tener que usar esto que da vuelta cuando quiere las notificaciones jaja
+      setPopUps(popUpsAux);
     }
-      
-  },[user])
+  }, [user]);
 
   useEffect(() => {
     console.log(sub);
     if (sub) dispatch(agregarSocker(sub));
   }, [sub]);
 
-  useEffect(()=>{
-    let cantidadAuxiliar = 0
-    popUps?.forEach(e => e.viewed?  cantidadAuxiliar: cantidadAuxiliar++  )
-    setcantNotificaciones(cantidadAuxiliar)
-  },[popUps,forceUpdate])
+  useEffect(() => {
+    let cantidadAuxiliar = 0;
+    popUps?.forEach((e) => (e.viewed ? cantidadAuxiliar : cantidadAuxiliar++));
+    setcantNotificaciones(cantidadAuxiliar);
+  }, [popUps, forceUpdate]);
 
-
-  useEffect(()=>{
-    socket?.on("obtenerNotificacion",({id,img,nombre_emisor,tipo}) => {
-      let popsAuxiliar = popUps
-      let popAuxiliar = {}
-      popAuxiliar = {type:tipo,viewed:false,img:img,name:nombre_emisor}
-      popsAuxiliar.push(popAuxiliar)
-      popsAuxiliar = popsAuxiliar.reverse()
-      setPopUps(popsAuxiliar)
-      let cantidadAuxiliar = 0
-      popsAuxiliar?.forEach(e => e.viewed?  cantidadAuxiliar: cantidadAuxiliar++  )
-      setcantNotificaciones(cantidadAuxiliar)
-    })
-  },[socket,popUps])
+  useEffect(() => {
+    socket?.on("obtenerNotificacion", ({ id, img, nombre_emisor, tipo }) => {
+      let popsAuxiliar = popUps;
+      let popAuxiliar = {};
+      popAuxiliar = {
+        type: tipo,
+        viewed: false,
+        img: img,
+        name: nombre_emisor,
+      };
+      popsAuxiliar.push(popAuxiliar);
+      popsAuxiliar = popsAuxiliar.reverse();
+      setPopUps(popsAuxiliar);
+      let cantidadAuxiliar = 0;
+      popsAuxiliar?.forEach((e) =>
+        e.viewed ? cantidadAuxiliar : cantidadAuxiliar++
+      );
+      setcantNotificaciones(cantidadAuxiliar);
+    });
+  }, [socket, popUps]);
 
   const handleCloseUserMenu = () => {
     setAnchorElUser(null);
@@ -131,22 +142,23 @@ const Profile = () => {
   const handleContracts = () => {
     navigate("/contracts/user/" + sub);
     setAnchorElUser(null);
-
   };
 
-  
   const showPopUps = () => {
-    setPopUpsEnabled(!popUpsEnabled)
-    let vistos = []
-    vistos = popUps.filter(e => !e.viewed)
-    vistos = vistos.map(e => e.id)
-    setPopUps(popUps.map(e => {return {...e,viewed:true}}))//Me pinto freestylear jaja
-    socket.emit("seen",vistos)
-    
-  }
+    setPopUpsEnabled(!popUpsEnabled);
+    let vistos = [];
+    vistos = popUps.filter((e) => !e.viewed);
+    vistos = vistos.map((e) => e.id);
+    setPopUps(
+      popUps.map((e) => {
+        return { ...e, viewed: true };
+      })
+    ); //Me pinto freestylear jaja
+    socket.emit("seen", vistos);
+  };
   const DshowPopUps = () => {
-    setPopUpsEnabled(false)
-  }
+    setPopUpsEnabled(false);
+  };
 
   const settings = [
     {
@@ -175,18 +187,21 @@ const Profile = () => {
   const handleClose = () => setOpen(false);
   return (
     <>
-    <div className={s.contenedor}>
-    <div className={s.badge}>
-    
-    <ClickAwayListener onClickAway={DshowPopUps}>
-      <div>
-      <Badge onClick={showPopUps} badgeContent={cantNotificaciones} color="primary">
-      <NotificationsNoneTwoToneIcon />
-      </Badge>
-      {popUpsEnabled? <PopUps popUps={popUps}/> : <></>}
-      </div>
-      </ClickAwayListener>
-    </div>
+      <div className={s.contenedor}>
+        <div className={s.badge}>
+          <ClickAwayListener onClickAway={DshowPopUps}>
+            <div>
+              <Badge
+                onClick={showPopUps}
+                badgeContent={cantNotificaciones}
+                color="primary"
+              >
+                <NotificationsNoneTwoToneIcon />
+              </Badge>
+              {popUpsEnabled ? <PopUps popUps={popUps} /> : <></>}
+            </div>
+          </ClickAwayListener>
+        </div>
         <div className={s.but}>
           <Button onClick={handleOpen}>
             <FaHeart />
@@ -202,13 +217,18 @@ const Profile = () => {
             </Box>
           </Modal>
         </div>
-    <div>
-        <Chip className={s.name} label={`${users.name} ${users.lastName}`} variant="outlined" />
-        <Tooltip title="Open settings">
-          <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-            <Avatar alt="Remy Sharp" src={users.img} />
-          </IconButton>
-        </Tooltip>
+        <div>
+          <Chip
+            className={s.name}
+            label={`${users.name} ${users.lastName}`}
+            variant="outlined"
+          />
+          <Tooltip title="Open settings">
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar alt="Remy Sharp" src={users.img} />
+            </IconButton>
+          </Tooltip>
+        </div>
       </div>
     </>
   );
