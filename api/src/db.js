af9fc900043b94a79c17122c79fd03fba4281461
Manu <manuel.canavari43@gmail.com>
2022-10-14 00:10:53 -0300

    chat bebe

--- api/src/db.js
+++ api/src/db.js
@@ -26,6 +26,7 @@ fs.readdirSync(path.join(__dirname, "/models"))
   });
 
 // Injectamos la conexion (sequelize) a todos los modelos
+
 modelDefiners.forEach((model) => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
 let entries = Object.entries(sequelize.models);
@@ -37,7 +38,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados, PopUp como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Admin, Chat, Contract, Job, User, Worker, Country, PopUp } = sequelize.models;
+const { Admin, Chat, Message, Contract, Job, User, Worker, Country, PopUp } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
@@ -47,12 +48,6 @@ Contract.belongsTo(User);
 
 Worker.hasMany(Contract);
 Contract.belongsTo(Worker);
-
-User.hasMany(Chat);
-Chat.belongsTo(User);
-
-Worker.hasMany(Chat);
-Chat.belongsTo(Worker);
 
 Country.hasMany(User);
 User.belongsTo(Country);
@@ -69,6 +64,21 @@ Worker.belongsToMany(Job, { through: "Works_Jobs" });
 // User.hasMany(PopUp)
 PopUp.belongsTo(User,{as:"Emiter"})
 PopUp.belongsTo(User,{as:"Receiver"})
+
+User.hasMany(Chat);
+Chat.belongsTo(User);
+
+Worker.hasMany(Chat);
+Chat.belongsTo(Worker);
+
+User.hasMany(Message);
+Message.belongsTo(User);
+
+Worker.hasMany(Message);
+Message.belongsTo(Worker);
+
+Chat.hasMany(Message);
+Message.belongsTo(Chat)
 
 
 
