994c8599bf62b8dc54375254ed0f79095e37d736
Santino Cattaneo <90813668+sonnyrmt@users.noreply.github.com>
2022-10-19 21:00:42 -0300

    fixes and styles

--- client/src/components/HomePrueba/HomePrueba.jsx
+++ client/src/components/HomePrueba/HomePrueba.jsx
@@ -6,6 +6,7 @@ import SearchBar from "../SearchBar/SearchBar";
 import Filters from "../Filters/Filters";
 import s from "./HomePrueba.module.css";
 import CardsWorkers from "../CardsWorkers/CardsWorkers";
+import Footer from "../Footer/Footer";
 import Pagination from "@mui/material/Pagination";
 
 export default function HomePrueba() {
@@ -90,7 +91,19 @@ export default function HomePrueba() {
               })}
           </div>
         </div>
-        <div className={s.footer}></div>
+        <div className={s.pagination}>
+          <Pagination
+            defaultPage={1}
+            color="primary"
+            count={numberPages}
+            page={page}
+            onChange={pagesNumber}
+            shape="rounded"
+          />
+        </div>
+        <div className={s.footer}>
+          <Footer />
+        </div>
       </div>
     </div>
   );
